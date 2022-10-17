d2ce5812b94f1428cd52c3875df523782b746967
yondo123 <46988995+yondo123@users.noreply.github.com>
2022-10-17 23:56:52 +0900

    ➕ Style Component 추가

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <StyleComponents />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+import StyleComponents from './components/StyleComponents.vue';
 
 export default {
     name: 'App',
     components: {
-        HelloWorld
+        HelloWorld,
+        StyleComponents
     }
 };
 </script>
 
 <style lang="scss">
 .title {
-    color: $font;
 }
 </style>
---
+++ src/components/StyleComponents.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="btn-sm-wrap"></div>
+    <div class="btn-lg-wrap">
+        <h2 class="font-lg">Button Component (Large Size)</h2>
+        <button class="btn-lg btn-kakao">
+            카카오 로그인
+            <i class="ic-kakao"></i>
+        </button>
+        <button class="btn-lg btn-naver">
+            네이버 로그인
+            <i class="ic-naver"></i>
+        </button>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.font-lg {
+    @include text-style(24);
+}
+
+.font-md {
+    @include text-style(16);
+}
+
+.font-lg {
+    @include text-style(24);
+}
+
+.btn-lg-wrap {
+    @include column-flexbox();
+    button {
+        margin-top: 8px;
+    }
+
+    .btn-lg {
+        color: $white;
+        background-color: $black;
+        i {
+            padding-left: 16px;
+            margin-bottom: 4px;
+        }
+    }
+
+    .ic-kakao {
+        color: $main;
+    }
+
+    .ic-naver {
+        color: $green;
+    }
+}
+</style>
