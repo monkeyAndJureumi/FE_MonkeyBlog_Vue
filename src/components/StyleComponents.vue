94c9205aa15f68aa0e651679175840f24a0b6760
yondo123 <46988995+yondo123@users.noreply.github.com>
2022-10-18 23:19:58 +0900

    ✨ Small Button 스타일 구현

--- src/components/StyleComponents.vue
+++ src/components/StyleComponents.vue
@@ -1,6 +1,15 @@
 <template>
-    <div class="btn-sm-wrap"></div>
-    <div class="btn-lg-wrap">
+    <div class="col">
+        <h2 class="font-lg">Icons</h2>
+        <div class="row icon-wrap">
+            <i class="ic-search"> </i>
+            <i class="ic-naver"> </i>
+            <i class="ic-kakao"> </i>
+            <i class="ic-menu"> </i>
+        </div>
+    </div>
+    <hr />
+    <div class="btn-lg-wrap col">
         <h2 class="font-lg">Button Component (Large Size)</h2>
         <button class="btn-lg btn-kakao">
             카카오 로그인
@@ -10,6 +19,19 @@
             네이버 로그인
             <i class="ic-naver"></i>
         </button>
+    </div>
+    <hr />
+    <div class="btn-sm-wrap col">
+        <h2 class="font-lg">Buttons (Small Size)</h2>
+        <h3 class="font-lg">disabled</h3>
+        <button class="btn-sm btn-pos" disabled>확인</button>
+        <button class="btn-sm btn-active" disabled>수정</button>
+        <button class="btn-sm btn-caution" disabled>삭제</button>
+
+        <h3 class="font-lg">active</h3>
+        <button class="btn-sm btn-pos">확인</button>
+        <button class="btn-sm btn-active">수정</button>
+        <button class="btn-sm btn-caution">삭제</button>
     </div>
 </template>
 
@@ -30,8 +52,22 @@ export default {};
     @include text-style(24);
 }
 
-.btn-lg-wrap {
+.col {
     @include column-flexbox();
+    padding: 32px 0;
+}
+
+.row {
+    @include flexbox(start);
+}
+
+.icon-wrap {
+    i {
+        margin-left: 8px;
+    }
+}
+
+.btn-lg-wrap {
     button {
         margin-top: 8px;
     }
@@ -41,7 +77,7 @@ export default {};
         background-color: $black;
         i {
             padding-left: 16px;
-            margin-bottom: 4px;
+            margin-bottom: 2px;
         }
     }
 
@@ -51,6 +87,12 @@ export default {};
 
     .ic-naver {
         color: $green;
+    }
+}
+
+.btn-sm-wrap {
+    button {
+        margin-top: 8px;
     }
 }
 </style>
